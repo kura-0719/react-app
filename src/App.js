ed8eaf69d5a226bebc5525ab50455527120c8a78
takumi <takura010719@gmail.com>
2022-10-08 21:46:46 +0900

    documentメソッド

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import './App.css';
 
 function App() {
 
-  function confirmed(fn) {
-    const input = window.prompt("実行しますか?");
-      fn(input);
-  }
+  const btn = document.getElementById("button");
 
-  confirmed(function (input) {
-    if (input === "実行")
-      console.log("リポジトリを削除")
+  btn.addEventListener("click",function() {
+    console.log("フォロー解除");
   });
 
   return (
