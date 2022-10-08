901d9a860e989dff3d9fc16c69d23774ebd7d541
takumi <takura010719@gmail.com>
2022-10-08 20:12:15 +0900

    変数について

--- src/App.js
+++ src/App.js
@@ -3,42 +3,12 @@ import './App.css';
 
 function App() {
 
-  class Student {
-    constructor(name){
-      this.name = name;
-    }
-    cal_avg(data) {
-      let sum = 0;
-      for (let i = 0; i < data.length; i++){
-      sum += data[i];
-      }
+  // 変数について
+  const foo =document.getElementById("foo");
 
-    let avg = sum / data.length;
-    return avg;
-    }
-
-    judge(avg) {
-      let result;
-      if (60 <= avg) {
-        result = "passed";
-      } else {
-        result = "failed";
-      }
-      return result;
-    }
-  }
-
-  let a001 = new Student("Sato");
-  let data = [70, 65, 50, 90, 30];
-  let avg = a001.cal_avg(data);
-  let result = a001.judge(avg);
-
-  console.log(data.length);
-  console.log(a001.name);
-  console.log(avg);
-  console.log(result);
-
-    
+  const fooWidth = foo.offsetWidth;
+  const fooHeight = foo.offsetHeight;
+  
   return (
     <div className="App">
       <header className="App-header">
